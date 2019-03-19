c350de4878a62e30573d9e23dcc51f92d6c5958f
ext2 <ext2@qq.com>
2019-03-19 22:47:54 +0800

    增加互斥量，保证程序只能运行一个实例。

--- PowerOff/Program.cs
+++ PowerOff/Program.cs
@@ -6,6 +6,8 @@ namespace PowerOff
 {
     static class Program
     {
+        private static System.Threading.Mutex mutex;
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -14,7 +16,17 @@ namespace PowerOff
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+
+            mutex = new System.Threading.Mutex(true, "OnlyOneInstance");
+            if (mutex.WaitOne(0, false))
+            {
+                Application.Run(new frmMain());
+            }
+            else
+            {
+                MessageBox.Show("程序已经在运行中。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
         }
     }
 }
